6d0421f9517ae6a31089f1bd4bd549fe1c2f4559
Patrick Melo <patrickabimael@yahoo.com.br>
2024-06-20 11:04:03 -0300

    primeiro commit

---
+++ src/main/java/com/sessao/SessaoApplication.java
@@ -0,0 +1,13 @@
+package com.sessao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessaoApplication.class, args);
+    }
+
+}
